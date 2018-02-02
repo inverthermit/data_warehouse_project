70fc45aeb69375844de18c55632c289445c14a64
Tim <l1111j@live.com>
2018-02-02 18:36:14 +1100

    Update: initial upload, initial structure

---
+++ Second_ETL.py
@@ -0,0 +1,36 @@
+import mysql.connector as sql
+# pip install mysql-connector-python
+import pandas as pd
+
+from Util import Util
+
+host = Util.getConfig('mysql_host')
+database = Util.getConfig('data_warehouse_schema')
+username = Util.getConfig('username')
+password = Util.getConfig('password')
+
+db_connection = sql.connect(host=host, database=database,
+                            user=username, password=password)
+df = pd.read_sql('SELECT * FROM dw_workshop5.customer1', con=db_connection)
+print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""End of file"""
